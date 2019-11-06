f01dac20383517d64527cc6e0a1666e1fa45723d
tanaka_yui <tanaka_yui@cyberagent.co.jp>
2019-11-06 18:10:43 +0900

    fix

--- prison.cpp
+++ prison.cpp
@@ -2,18 +2,14 @@
 
 int main()
 {
-  int n, m;
+  int n, m, i, l, r = 0;
   if (scanf("%d %d", &n, &m) == -1) return -1;
-  int lr[2][m];
-  int i = 0;
-  for(i = 0;i < m;i++) {
-    if (scanf("%d %d", &lr[0][i], &lr[1][i]) == -1) return -1;
-  }
   int left = 1;
   int right = 1000000;
-  for(i=0;i<m;i++) {
-    if (left < lr[0][i]) left = lr[0][i];
-    if (right > lr[1][i]) right = lr[1][i];
+  for(i = 0;i < m;i++) {
+    if (scanf("%d %d", &l, &r) == -1) return -1;
+    if (left < l) left = l;
+    if (right > r) right = r;
   }
   int ans = right - left + 1;
   printf("%d\n", ans < 0 ? 0 : ans);
